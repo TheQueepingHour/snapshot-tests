9a3267c44d140b7ff50bada6a7e291b09e98ec5a
Josh Rivera <beps@Mactop.local>
2024-03-05 19:37:10 -0600

    Added style

--- src/GitHubCard.js
+++ src/GitHubCard.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card'
 export default function GitHubCard() {
     return(
         <div>
-            <Card>
+            <Card style={{ width: '18rem' }}>
                 <Card.Img variant='top' src='../public/Subject.png' />
                 <Card.Body>
                     <Card.Title>TheQueepingHour</Card.Title>
